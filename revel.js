6d49120030ca8fd92ea1283b9546d6e3dd268c5d
DiegoNotreve <notreveogeide@gmail.com>
2025-02-15 14:15:41 -0300

    finalizado a parte web

---
+++ revel.js
@@ -0,0 +1,26 @@
+window.revelar = ScrollReveal({ reset: true });
+
+revelar.reveal(".efeito-logo", { duration: 2000, distance: "-200px" });
+revelar.reveal(".efeito-frase", { duration: 2000, distance: "-200px" });
+
+revelar.reveal(".efeito-servicos", { duration: 1500, distance: "200px", origin:'left' });
+
+revelar.reveal("#efeito-servicos-subtitle", { duration: 2000, distance: '100px', origin: 'left', delay: 700,  });
+
+// revelar.reveal("#efeito-card1", { duration: 3000, distance: "100px", origin: 'left', delay: 500 });
+
+// revelar.reveal("#efeito-card2", { duration: 3000, distance: "100px", origin: 'left', delay: 1000 });
+
+// revelar.reveal("#efeito-card3", { duration: 3000, distance: "100px", origin: 'left', delay: 1500 });
+
+// revelar.reveal(".efeito-importancia", { duration: 2000, distance: '100px' });
+
+revelar.reveal(".efeito-container-importancia1", { duration: 2000, distance: '200px', origin: 'left', delay: 500,  });
+
+revelar.reveal(".efeito-container-importancia2", { duration: 2000, distance: '200px', origin: 'left', delay: 1000,  });
+
+revelar.reveal(".efeito-container-importancia3", { duration: 2000, distance: '200px', origin: 'left', delay: 1200,  });
+
+revelar.reveal(".efeito-sobre", { duration: 2000, distance: '100px', origin:'left', delay:500 });
+
+revelar.reveal(".efeito-texto-sobre", { duration: 2000, distance: '100px', origin: 'left', delay:1000});
